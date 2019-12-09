5244d59313657d88785dcf608a0f0865fe690ace
Christian Neubauer <christian.neubauer@1und1.de>
2019-12-09 12:42:41 +0100

    Module namespace changed

--- lib/spectre/diagnostic.rb
+++ lib/spectre/diagnostic.rb
@@ -1,25 +1,29 @@
-module Spectre::Diagnostic
-  @@duration = 0.0
+module Spectre
+  module Diagnostic
+    module Stopwatch
+      @@duration = 0.0
 
-  class << self
-    def start
-      @@start_time = Time.now
-    end
+      class << self
+        def start
+          @@start_time = Time.now
+        end
 
-    def stop
-      @@end_time = Time.now
-    end
+        def stop
+          @@end_time = Time.now
+        end
 
-    def measure
-      start
-      yield
-      stop
-    end
+        def measure
+          start
+          yield
+          stop
+        end
 
-    def duration
-      @@end_time - @@start_time
+        def duration
+          @@end_time - @@start_time
+        end
+      end
+
+      Spectre.delegate :start, :stop, :duration, :measure, to: self
     end
   end
-
-  Spectre.delegate :start, :stop, :duration, :measure, to: self
 end
