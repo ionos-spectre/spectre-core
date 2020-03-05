3f1640a511e8b7c298954960544a789b36a70a6d
Christian Neubauer <christian.neubauer@ionos.com>
2020-03-05 08:39:58 +0100

    Empty assertions added

--- lib/spectre/assertion.rb
+++ lib/spectre/assertion.rb
@@ -3,8 +3,16 @@ class Object
       raise Spectre::ExpectationFailure.new(val, self) unless self.to_s == val.to_s
   end
 
+  def should_be_empty
+    raise Spectre::ExpectationFailure.new(val, self) unless self == nil
+  end
+
   def should_not_be(val)
       raise Spectre::ExpectationFailure.new(val, self) unless self.to_s != val.to_s
+  end
+
+  def should_not_be_empty
+    raise Spectre::ExpectationFailure.new(val, self) unless self != nil
   end
 end
 
@@ -19,5 +27,13 @@ end
 class Array
   def should_contain(val)
       raise Spectre::ExpectationFailure.new(val, self) unless self.include? val
+  end
+
+  def should_be_empty
+    raise Spectre::ExpectationFailure.new(val, self) unless self.length == 0
+  end
+
+  def should_not_be_empty
+    raise Spectre::ExpectationFailure.new(val, self) unless self.length > 0
   end
 end
