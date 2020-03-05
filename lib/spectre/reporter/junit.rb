58e4e1ac51590801a2a808e1f2876666075cae4d
Christian Neubauer <christian.neubauer@ionos.com>
2020-03-05 08:40:09 +0100

    JUnit report updated

--- lib/spectre/reporter/junit.rb
+++ lib/spectre/reporter/junit.rb
@@ -17,22 +17,34 @@ module Spectre::Reporter
           xml_str += '<testcase id="' + run_info.spec.name + '" name="' + run_info.spec.desc + '" time="' + ('%.3f' % run_info.duration) + '">'
 
           if run_info.spec.error
+            text = nil
+
             if run_info.spec.error.cause
               if run_info.spec.error.cause.is_a? Spectre::ExpectationFailure
-                failure = "failed with #{run_info.spec.error.cause.failure || 'nil'}"
+                failure = "Expected #{run_info.spec.error}"
+                failure += " with #{run_info.data}" if run_info.data
+
+                if run_info.spec.error.cause.failure
+                  failure += " but it failed with #{run_info.spec.error.cause.failure}"
+                else
+                  failure += " but it failed"
+                end
+
                 type = 'FAILURE'
               else
                 failure = run_info.spec.error.cause.message
                 type = 'ERROR'
               end
+
               text = run_info.spec.error.cause.backtrace.join "\n"
             else
               failure = run_info.spec.error.message
               type = 'ERROR'
               text = run_info.spec.error.backtrace.join "\n"
             end
+
             xml_str += '<failure message="' + failure.gsub('"', '`') + '" type="' + type + '">'
-            # xml_str += '<![CDATA[' + text + ']]>'
+            xml_str += '<![CDATA[' + text + ']]>' if text
             xml_str += '</failure>'
           end
 
