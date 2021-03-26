0618562a4dfcfcb097c06ccb4b0136d13471241c
Christian Neubauer <christian.neubauer@ionos.com>
2021-03-26 13:22:37 +0100

    Logger module added

---
+++ lib/spectre/logger.rb
@@ -0,0 +1,10 @@
+module Spectre::Logger
+  module Status
+    OK = '[ok]'.green
+    FAILED = '[failed]'.red
+    ERROR = '[error]'.red
+    INFO = '[info]'.blue
+    SKIPPED = '[skipped]'.grey
+    DEBUG = '[debug]'.grey
+  end
+end
