2d4a4c6f4827f91344a0b0f68dff8ef93045d9a5
Christian Neubauer <christian.neubauer@ionos.com>
2021-04-22 16:22:21 +0200

    Additional fatal tests added

--- testing/tests/specs/assertion.spec.rb
+++ testing/tests/specs/assertion.spec.rb
@@ -1,6 +1,19 @@
 describe 'spectre/assertion' do
+  context 'fails' do
+    it 'compares two equal strings and fails', tags: [:assertion, :equal, :fatal] do
+      expect 'two strings not to be equal' do
+        'foo'.should_not_be 'foo'
+      end
+    end
 
-  it 'expects code to pass', tags: [:assertion, :core] do
+    it 'checks string values with grouped boolean operation', tags: [:assertion, :contain, :bool, :fatal] do
+      expect '(foo and bar) or (bla and blubb) to be contained' do
+        'This text contains bla foo'.should_contain ('foo'.and 'bar').or ('bla'.and 'blubb')
+      end
+    end
+  end
+
+  it 'expects code to pass', tags: [:assertion] do
     expect 'two strings to be equal' do
       'foo'.should_be 'foo'
     end
@@ -10,7 +23,7 @@ describe 'spectre/assertion' do
     end
   end
 
-  it 'compares two unequal strings and fails', tags: [:assertion, :equal, :core, :fail] do
+  it 'compares two unequal strings and fails', tags: [:assertion, :equal, :fail] do
     observe do
       'foo'.should_not_be 'foo'
     end
@@ -20,7 +33,7 @@ describe 'spectre/assertion' do
     end
   end
 
-  it 'compares two equal strings and fails', tags: [:assertion, :equal, :core, :fail] do
+  it 'compares two equal strings and fails', tags: [:assertion, :equal, :fail] do
     observe do
       'foo'.should_be 'bar'
     end
@@ -30,7 +43,7 @@ describe 'spectre/assertion' do
     end
   end
 
-  it 'looks up a string which contains another one but it fails', tags: [:assertion, :contain, :core, :fail] do
+  it 'looks up a string which contains another one but it fails', tags: [:assertion, :contain, :fail] do
     observe do
       "this is a pretty long text, that contains bar".should_contain 'foo'
     end
@@ -40,7 +53,7 @@ describe 'spectre/assertion' do
     end
   end
 
-  it 'observes if an item is in a list and fails', tags: [:assertion, :contain, :array, :core, :fail] do
+  it 'observes if an item is in a list and fails', tags: [:assertion, :contain, :array, :fail] do
     observe do
       ['foo', 'bla'].should_contain 'bar'
     end
@@ -50,7 +63,7 @@ describe 'spectre/assertion' do
     end
   end
 
-  it 'observes a process which succeeds', tags: [:assertion, :observe, :core] do
+  it 'observes a process which succeeds', tags: [:assertion, :observe] do
     observe 'some process' do
       log 'do some stuff'
     end
@@ -60,7 +73,7 @@ describe 'spectre/assertion' do
     end
   end
 
-  it 'observes a process which fails', tags: [:assertion, :observe, :core] do
+  it 'observes a process which fails', tags: [:assertion, :observe] do
     observe 'some process' do
       raise 'There was a problem'
     end
@@ -70,13 +83,13 @@ describe 'spectre/assertion' do
     end
   end
 
-  it 'checks string values with boolean operation', tags: [:assertion, :contain, :bool, :core] do
+  it 'checks string values with boolean operation', tags: [:assertion, :contain, :bool] do
     expect 'foo or bar or (bla and blubb) to be contained' do
       'This text contains bla blubb'.should_contain 'foo'.or 'bar'.or 'bla'.and 'blubb'
     end
   end
 
-  it 'checks string values with boolean operation, but it fails', tags: [:assertion, :contain, :bool, :core, :fail] do
+  it 'checks string values with boolean operation, but it fails', tags: [:assertion, :contain, :bool, :fail] do
     observe do
       'This text contains bla'.should_contain 'foo'.or 'bar'.or 'bla'.and 'blubb'
     end
@@ -86,20 +99,20 @@ describe 'spectre/assertion' do
     end
   end
 
-  it 'checks string values with grouped boolean operation', tags: [:assertion, :contain, :bool, :core] do
+  it 'checks string values with grouped boolean operation', tags: [:assertion, :contain, :bool] do
     expect '(foo and bar) or (bla and blubb) to be contained' do
       'This text contains bar foo'.should_contain ('foo'.and 'bar').or ('bla'.and 'blubb')
     end
   end
 
   context 'use of custom operators' do
-    it 'checks string values with grouped boolean operation', tags: [:assertion, :contain, :bool, :core] do
+    it 'checks string values with grouped boolean operation', tags: [:assertion, :contain, :bool] do
       expect '(foo and bar) or (bla and blubb) to be contained' do
         'This text contains bar foo'.should_contain ('foo' & 'bar') | ('bla' & 'blubb')
       end
     end
 
-    it 'checks string values with grouped boolean operation, but it fails', tags: [:assertion, :contain, :bool, :core, :fail] do
+    it 'checks string values with grouped boolean operation, but it fails', tags: [:assertion, :contain, :bool, :fail] do
       observe do
         'This text contains bar blubb'.should_contain ('foo' & 'bar') | ('bla' & 'blubb')
       end
@@ -110,7 +123,7 @@ describe 'spectre/assertion' do
     end
   end
 
-  it 'checks integer values with grouped boolean operation', tags: [:assertion, :contain, :bool, :core] do
+  it 'checks integer values with grouped boolean operation', tags: [:assertion, :contain, :bool] do
     expect '(1 and 2) or (3 and 4) to be contained' do
       'This text contains 1 2'.should_contain (1.and 2).or (3.and 4)
     end
