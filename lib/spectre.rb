b009e765cf8fff9f79fcdb1bb61a73378eea916c
Christian Neubauer <christian.neubauer@ionos.com>
2021-11-24 08:58:36 +0100

    Deep merge method moved

--- lib/spectre.rb
+++ lib/spectre.rb
@@ -8,6 +8,14 @@ module Spectre
   VERSION = [Version::MAJOR, Version::MINOR, Version::TINY].compact * '.'
 
 
+  class ::Hash
+    def deep_merge!(second)
+      merger = proc { |key, v1, v2| Hash === v1 && Hash === v2 ? v1.merge!(v2, &merger) : v2 }
+      self.merge!(second, &merger)
+    end
+  end
+
+
   ###########################################
   # Custom Exceptions
   ###########################################
