bf62244bc8eaf9426f2546e91b8651e7e75a42e5
Christian Neubauer <christian.neubauer@ionos.com>
2021-11-25 14:46:51 +0100

    to_h of Object class fixed

--- lib/spectre.rb
+++ lib/spectre.rb
@@ -19,7 +19,7 @@ module Spectre
     end
   end
 
-  class Object
+  class ::Object
     def to_h
       self.instance_variables.each_with_object({}) do |var, hash|
         hash[var.to_s.delete("@")] = self.instance_variable_get(var)
