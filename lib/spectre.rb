d789fa587384b4422d0159beba27faa57f575355
Christian Neubauer <christian.neubauer@ionos.com>
2025-02-10 12:45:36 +0100

    Use different colors for context description

--- lib/spectre.rb
+++ lib/spectre.rb
@@ -460,7 +460,7 @@ module Spectre
     #
     def describe contexts, level = 0
       contexts.each do |context|
-        @out.puts("#{'  ' * level}#{context.desc.blue}")
+        @out.puts("#{'  ' * level}#{context.desc.send(level.positive? ? :cyan : :blue)}")
 
         context.specs.each do |spec|
           @out.puts("#{'  ' * (level + 1)}#{spec.desc}")
