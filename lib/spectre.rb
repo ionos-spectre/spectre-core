5a73f8be5819d8087020e04658f7aee04c9c43bf
Christian Neubauer <christian.neubauer@ionos.com>
2025-02-19 09:38:56 +0100

    Update version

--- lib/spectre.rb
+++ lib/spectre.rb
@@ -1,8 +1,8 @@
 module Spectre
   module Version
     MAJOR = 1
-    MINOR = 14
-    TINY  = 6
+    MINOR = 15
+    TINY  = 0
   end
 
   VERSION = [Version::MAJOR, Version::MINOR, Version::TINY].compact * '.'
